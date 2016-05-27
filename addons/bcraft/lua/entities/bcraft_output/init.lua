c4c73b18ee8c153a80667497167adb12089ee37d
Billard <foresome1249@gmail.com>
2016-05-26 15:50:13 -1000

    Update init.lua

--- addons/bcraft/lua/entities/bcraft_output/init.lua
+++ addons/bcraft/lua/entities/bcraft_output/init.lua
@@ -1,1 +1,31 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetUseType(SIMPLE_USE)
+	local phys = self:GetPhysicsObject()
+
+	if not phys:IsValid() then
+		self:SetModel("models/weapons/w_rif_ak47.mdl")
+		self:PhysicsInit(SOLID_VPHYSICS)
+		phys = self:GetPhysicsObject()
+    end
+
+	phys:Wake()
+end
+
+function ENT:OnTakeDamage(dmg)
+	self:TakePhysicsDamage(dmg)
+end
+
+function ENT:Use(activator, caller)
+	if !IsValid(caller) or !IsPlayer(caller) then return end
+	
+end
+	
 
