3d5f170d2e9c270367006690934547bc634beaf0
Billard <foresome1249@gmail.com>
2016-05-26 19:48:20 -1000

    All I needed was one stupid command the whole time...

--- addons/bcraft_v2/lua/autorun/server/sv_commands.lua
+++ addons/bcraft_v2/lua/autorun/server/sv_commands.lua
@@ -1,10 +1,13 @@
-include("bcraft_config.lua")
+BCraft.RecipeList = {
+	["000002lockpick"] = true,
+	["020301m9k_ak47"] = true,
+}
 
 hook.Add("PlayerSay", "BCraft_CraftCommand", function(ply, text, team)
 	if !IsValid(ply) or !IsPlayer(ply) then return end
 	if string.sub(text, 1, 6) == "/craft" or string.sub(text, 1, 6) == "!craft" then
 		if string.len(text) < 8 then
-			ply:ChatPrint("Please specify what you would like to craft, or specify 'list' for a list of items you can craft.")
+			ply:ChatPrint("Please specify what you would like to craft, or specify 'list' for a list of craftable items.")
 			return
 		end
 		local CraftItem = string.sub(text, 8, string.len(text))
@@ -20,7 +23,11 @@ hook.Add("PlayerSay", "BCraft_CraftCommand", function(ply, text, team)
 		for k, v in pairs(BCraft.RecipeList) do
 			if CraftItem == k then
 				if !v then return end
-				local Wood, Springs, Wrenches, Classname
+				local Wood, Springs, Wrenches, Classname = BCraft.DecodeRecipe(k)
+				local PlyWood, PlySprings, PlyWrenches = ply:GetBCraftSupply()
+				if PlyWood >= Wood and PlySprings >= PlySprings and PlyWrenches >= Wrenches then
+					
+				end
 			end
 		end
 	end
