fa4d8d52969ddc6af2b2c04713e10b145c03b5b7
Billard <foresome1249@gmail.com>
2016-05-27 17:59:43 -1000

    Update sv_perks.lua

--- addons/bperks/lua/autorun/sv_perks.lua
+++ addons/bperks/lua/autorun/sv_perks.lua
@@ -1,1 +1,40 @@
---Main file for the perk's effects
+--[[
+
+List of Perks:
+diehard - Explode on death
+groundpound - Deal splash damage when hitting the group
+feather - Take no fall damage
+arson - Ignite targets
+
+]]--
+
+BPerks = {}
+
+BPerks.PerkList = {
+	["diehard"] = true,
+	["groundpound"] = true,
+	["feather"] = true,
+	["arson"] = true,
+}
+
+local PlyMeta = FindMetaTable("Player")
+
+function PlyMeta:HasBPerk(perk)
+	return self:GetPData("bperk_"..perk, 0) > 0
+end
+
+function PlyMeta:GiveBPerk(perk)
+	self:SetPData("bperk_"..perk, 1)
+end
+
+function PlyMeta:TakeBPerk(perk)
+	self:SetPData("bperk_"..perk, 0)
+end
+
+function PlyMeta:TakeAllBPerks()
+	for k, v in pairs(BPerks.PerkList) do
+		if v then
+			self:SetPData("bperk_"..tostring(k), 0)
+		end
+	end
+end
